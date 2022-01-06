a0724d87fb784fa2523a773d3b00d9e2845ca802
Cell <suhail9453531504@gmail.com>
2022-01-06 11:13:43 +0530

    Added list & detail pages for products; 1/4

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -14,11 +15,12 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product-list_by_category', args=[self.slug])
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
