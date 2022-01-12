0c9edc671a331957927cd738075a5784bb52984a
Cell <suhail9453531504@gmail.com>
2022-01-12 22:05:45 +0530

    Updated orders/admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -3,8 +3,6 @@ from .models import Order, OrderItem
 import csv
 import datetime
 from django.http import HttpResponse
-import zipfile
-import io
 
 
 class OrderItemInline(admin.TabularInline):
@@ -12,36 +10,16 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
-def zipfiles(files):
-    with zipfile.ZipFile('orders.zip', 'w') as zipObj:
-        for file in files:
-            zipObj.write(file)
-
-
 def export_to_csv(modeladmin, request, queryset):
-    opts = modeladmin.model._meta   # opts = options
-    output = io.BytesIO()
-
-    # files = []
-    # content_disposition = []
-    # for order in queryset:
-    #     filename = f'OrderID: {order.id}.csv'
-    #     cd = f'attachment; filename={filename}'
-    #     files.append(filename)
-    #     content_disposition.append(cd)
-
+    opts = modeladmin.model._meta
     content_disposition = f'attachment; filename={opts.verbose_name}.csv'
     response = HttpResponse(content_type='text/csv')
-
-    if len(queryset) > 1:
-        content_disposition = f'attachment; filename={opts.verbose_name}.csv.zip'
-        response = HttpResponse(content_type='application/zip')
-
     response['Content-Disposition'] = content_disposition
     writer = csv.writer(response)
+
     fields = [field for field in opts.get_fields() if not field.many_to_many
               and not field.one_to_many]
-    # Write a first row with header information including field names
+    # Write a first row with header information
     writer.writerow([field.verbose_name for field in fields])
     # Write data rows
     for obj in queryset:
@@ -52,13 +30,10 @@ def export_to_csv(modeladmin, request, queryset):
                 value = value.strftime('%d/%m/%Y')
             data_row.append(value)
         writer.writerow(data_row)
-
-    if len(queryset) > 1:
-        # Append individual csvs to this zip
-        z = zipfile.ZipFile(response, 'w')
-        z.writestr(f"anyone.csv", output.getvalue())
     return response
 
+
+export_to_csv.short_description = 'Export to CSV'
 
 export_to_csv.short_description = 'Export to CSV'   # customizing name
 
